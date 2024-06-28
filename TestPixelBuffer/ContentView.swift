a77870d1ee412d0127c958a3b5478b072c8c6393
sramp <sramp@ngi.it>
2024-06-28 17:17:37 +0200

    Initial Commit

---
+++ TestPixelBuffer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestPixelBuffer
+//
+//  Created by Stefano Rampazzo on 28/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestPixelBuffer/TestPixelBufferApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPixelBufferApp.swift
+//  TestPixelBuffer
+//
+//  Created by Stefano Rampazzo on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TestPixelBufferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
